57a351363b5e0a5dd0303b8660b588d4718276ca
Jorrit Gerets <contact@jorrit.info>
2021-04-25 21:44:40 +0200

    Update flags

--- include/flags.h
+++ include/flags.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // These will change, don't worry
-#define CTF_FLAG1 "UHCTF{D0ntL0g3v3ryth1ng}"
-#define CTF_FLAG2 "UHCTF{D1sabl3B00tP1ns}"
-#define CTF_FLAG3 "UHCTF{YayW1f1C0nn3ct3d}"
-#define CTF_FLAG4 "UHCTF{3ncryptTr4ff1c}"
-static const char *CTF_FLAG5 __attribute__((used)) = "UHCTF{D0ntL34kK3ys}";
+#define CTF_FLAG1 "UHCTF{D0ntL0g3v3ryth1ng4551}"
+#define CTF_FLAG2 "UHCTF{D1sabl3B00tP1ns9568}"
+#define CTF_FLAG3 "UHCTF{YayW1f1C0nn3ct3d6488}"
+#define CTF_FLAG4 "UHCTF{3ncryptTr4ff1c6268}"
+static const char *CTF_FLAG5 __attribute__((used)) = "UHCTF{D0ntL34kK3ys1424}";
